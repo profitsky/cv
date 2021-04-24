5e4dbd177129f7177c1384f3e28790d808e14b65
piotr <peter.zyskowski@gmail.com>
2021-04-25 01:16:19 +0200

    Initial Issue

---
+++ bundler/webpack.config.dev.js
@@ -0,0 +1,15 @@
+const path = require("path");
+const { merge } = require('webpack-merge')
+const commonConfiguration = require('./webpack.common.js')
+
+module.exports = merge(
+    commonConfiguration, {
+    mode: "development",
+    
+    devServer:
+          {
+            open: true,
+            contentBase: path.resolve(__dirname, "../", "static")
+          }
+  }
+)
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import "../src/sass/main.scss"
+
+const message = "dziala jak skurwysyn";
+const btn = document.querySelector(".btn");
+btn.addEventListener("click", ()=>{
+    console.log("ssji")
+})
