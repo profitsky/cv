9e00ed4498ff9c968070eb4e2f04ef592f94dd7d
piotr <peter.zyskowski@gmail.com>
2021-05-09 00:29:19 +0200

    job history added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,1 @@
-import "../src/sass/main.scss"
-
-
-const message = "dziala jak skurwysyn";
-const btn = document.querySelector(".btn");
-btn.addEventListener("click", ()=>{
-    console.log("ssji")
-})
+import "../src/sass/main.scss";
